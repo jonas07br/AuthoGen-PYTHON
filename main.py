eec5a745011e8dd04f0d1c2055a2f6549b28d2c2
jonas07br <jonasrafa07@gmail.com>
2025-11-30 23:51:56 -0300

    feat: add model prediction with users input

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import chat_conversor
 import pandas as pd
 import utils
-import model
+import markov_chain_model
 import phrase_generator
+import naive_bayes_model
+
+
+
 # chat_conversor.convertChatToCsv('_chat.txt')
 
-def showPhrase(start:str):
-    print(phrase_generator.generate_hybrid_story(hybrid_markov,limit=10,start=start))
+# def showPhrase(start:str):
+#     print(phrase_generator.generate_hybrid_story(hybrid_markov,limit=10,start=start))
     
 gplaysDf = pd.read_csv("_chat.csv",encoding="utf-16")
 
-cleaned_mgs = utils.process_df_msgs(gplaysDf,'gabriel bocão',8)
+targets = ['Spot','Antony']
+markov_models = {}
+for target in targets:
+    cleaned_mgs = utils.process_df_msgs(gplaysDf,target,2)
+    markov_models[target] = markov_chain_model.make_hybrid_markov_model(cleaned_mgs,max_gram=3,target=target)
+    print(f"Modelo Markov para {target} criado.")
 
-# hybrid_markov = model.make_hybrid_markov_model()
-
-hybrid_markov = model.load_model_from_json('modelo_markov.json')
-
-# for i in range(1):
-#     print(phrase_generator.generate_hybrid_story(hybrid_markov,limit=10,start="eu quero ver esse"))
+nbModel,vectorizer = naive_bayes_model.make_naive_bayes_model(targets,gplaysDf)
+print("Modelo Naive Bayes criado.")
 while True:
-    start = input("Insira o texto: ")
-    showPhrase(start)
-    
+    user_input = input("Digite uma frase: ")
+    user_input_vectorized = vectorizer.transform([user_input])
+    predicted_author = nbModel.predict(user_input_vectorized)[0]
+    print(f"Autor previsto: {predicted_author}")
+    print("Frase gerada pelo modelo Markov:")
+    print(phrase_generator.generate_hybrid_story(markov_models[predicted_author],limit=10))
