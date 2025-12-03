fc12acc4622b5413ceee252b001e3f149e97f1de
jonas07br <jonasrafa07@gmail.com>
2025-12-03 18:31:18 -0300

    feat: update message processing parameters for Markov model and Naive Bayes model

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ gplaysDf = pd.read_csv("_chat.csv",encoding="utf-16")
 targets = ['Spot','Antony']
 markov_models = {}
 for target in targets:
-    cleaned_mgs = utils.process_df_msgs(gplaysDf,target,2)
+    cleaned_mgs = utils.process_df_msgs(gplaysDf,target,8)
     markov_models[target] = markov_chain_model.make_hybrid_markov_model(cleaned_mgs,max_gram=3,target=target)
     print(f"Modelo Markov para {target} criado.")
 
-nbModel,vectorizer = naive_bayes_model.make_naive_bayes_model(targets,gplaysDf)
+nbModel,vectorizer = naive_bayes_model.make_naive_bayes_model(targets,gplaysDf,minLength=6)
 print("Modelo Naive Bayes criado.")
 while True:
     user_input = input("Digite uma frase: ")
